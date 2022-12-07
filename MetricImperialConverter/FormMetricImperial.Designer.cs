6dbc035227961b4e81bbcb70acf13b1207a499a4
Kiril Spasov <spasov.kiril@gmail.com>
2022-12-07 11:33:24 +0200

    Add project files.

---
+++ MetricImperialConverter/FormMetricImperial.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace MetricImperialConverter
+{
+    partial class FormMetricImperial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnConvert = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnConvert
+            // 
+            this.BtnConvert.Location = new System.Drawing.Point(40, 56);
+            this.BtnConvert.Name = "BtnConvert";
+            this.BtnConvert.Size = new System.Drawing.Size(179, 62);
+            this.BtnConvert.TabIndex = 0;
+            this.BtnConvert.Text = "Convert";
+            this.BtnConvert.UseVisualStyleBackColor = true;
+            this.BtnConvert.Click += new System.EventHandler(this.BtnConvert_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(243, 56);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(483, 341);
+            this.TxtResult.TabIndex = 1;
+            // 
+            // FormMetricImperial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnConvert);
+            this.Name = "FormMetricImperial";
+            this.Text = "Metric Imperial Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnConvert;
+        private System.Windows.Forms.TextBox TxtResult;
+    }
+}
+
---
+++ MetricImperialConverter/FormMetricImperial.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace MetricImperialConverter
+{
+    public partial class FormMetricImperial : Form
+    {
+        public FormMetricImperial()
+        {
+            InitializeComponent();
+        }
+
+        int totalMiles;
+        int totalYards;
+        int totalFeet;
+        int totalInches;
+
+        private void BtnConvert_Click(object sender, EventArgs e)
+        {
+            string line = "";
+            int cm;
+
+            string path = Application.StartupPath + @"\metric.txt";
+            StreamReader streamReader = new StreamReader(path);
+
+            bool finished = false;
+
+            while (!finished)
+            {
+                line = streamReader.ReadLine();
+
+                if (line == null)
+                {
+                    finished = true;
+                }
+                else
+                {
+                    cm = Convert.ToInt32(line);
+                    ConvertMetric(cm);
+                    TxtResult.Text += totalMiles + " mile(s), " + totalYards + " yard(s), " + totalFeet + " foo(ee)t, " + totalInches + " inch(es)" + Environment.NewLine;
+                }
+            }
+        }
+
+        private void ConvertMetric(int cm)
+        {
+            //Convert all to cm.
+            double inch = 2.54;
+            double foot = 12 * 2.54;
+            double yard = 3 * 12 * 2.54;
+            double mile = 1760 * 3 * 12 * 2.54;
+
+            double remaining;
+
+            totalMiles = (int)((double)cm / mile);
+            remaining = cm - totalMiles * mile;
+
+            totalYards = (int)((double)remaining / yard);
+            remaining -= totalYards * yard;
+
+            totalFeet = (int)((double)remaining / foot);
+            remaining -= totalFeet * foot;
+
+            totalInches = (int)((double)remaining / inch);
+            remaining -= totalInches * inch;
+        }
+    }
+}
